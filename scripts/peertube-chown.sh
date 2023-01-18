bafc0036e24c5138fe906c3a2e8703b208af4d80
unknown <prabhurikish@gmail.com>
2023-01-18 17:33:22 +0530

    peer

--- scripts/peertube-chown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Ensure user is ok for config files
-find /config ! -user peertube -exec chown peertube:peertube {} \;
-
-# Ensure user is ok for data files
-find /data ! -user peertube -exec chown peertube:peertube {} \;
-
-exit 0
